72d0f561b22e4c6334dda246c5fb47d903287f98
Sezer <sezer.boluk@niltekyazilim.com.tr>
2023-10-19 22:55:33 +0300

    google analitc

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,5 +102,30 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-FHWYM64MVG",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "kiliclarticaret.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 };
